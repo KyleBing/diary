c945f9ff0c28fa9e415c87d5497a12e4a5fbcbc0
KyleBing <kylebing@163.com>
2020-10-19 17:25:58 +0800

    配置开发和生产不同的请求地址

--- src/components/diaryListItem.vue
+++ src/components/diaryListItem.vue
@@ -4,8 +4,8 @@
       <span class="date">{{diary.date}}</span>
       <div class="detail">
          <p class="title">{{diary.title}}</p>
-         <img :alt="diary.content" v-if="diary.content" class="icon" src="/img/content.svg"/>
-         <img :alt="diary.weather" v-if="diary.weather" class="icon" :src="'/img/weather/' + diary.weather + '.svg'"/>
+         <img :alt="diary.content" v-if="diary.content" class="icon" src="img/content.svg"/>
+         <img :alt="diary.weather" v-if="diary.weather" class="icon" :src="'img/weather/' + diary.weather + '.svg'"/>
       </div>
    </router-link>
 </template>
