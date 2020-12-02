817cda34aaf0d5c77d76fda0b1ae5f02cf528a7b
KyleBing <kylebing@163.com>
2020-12-02 10:39:06 +0800

    ^ outputDir: ../diary

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: './',
   productionSourceMap: false, // 不产出 .map 文件
 
-  outputDir: 'diary',
+  outputDir: '../diary',
   devServer: {
     proxy: {
       '^/api': {
