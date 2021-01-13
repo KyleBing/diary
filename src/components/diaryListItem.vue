8a4a3c03bb71177774feaffdf29d762af57d4f72
KyleBing <kylebing@163.com>
2021-01-13 10:38:56 +0800

    active 时列表图标变为白色，替换end 图标

--- src/components/diaryListItem.vue
+++ src/components/diaryListItem.vue
@@ -1,11 +1,11 @@
 <template>
-   <router-link :to="`/detail/${diary.id}`" :class="['list-item', 'diary-list-item-' + diary.category, {active: this.$route.params.id === diary.id}]" >
+   <router-link :to="`/detail/${diary.id}`" :class="['list-item', 'diary-list-item-' + diary.category, {active: active}]" >
       <i :class="['category', 'bg-' + diary.category]"></i>
       <span class="date">{{diary.date}}</span>
       <div class="detail">
          <p class="title">{{diary.title}}</p>
-         <img alt="Content" v-if="diary.content" class="icon" src="img/content.svg"/>
-         <img :alt="diary.weather" v-if="diary.weather" class="icon" :src="'img/weather/' + diary.weather + '.svg'"/>
+         <img alt="Content" v-if="diary.content" class="icon" :src="`img/content${suffix}.svg`"/>
+         <img :alt="diary.weather" v-if="diary.weather" class="icon" :src="`img/weather/${diary.weather}${suffix}.svg`"/>
       </div>
    </router-link>
 </template>
@@ -15,6 +15,14 @@
       name: "diaryListItem",
       props: {
          diary: Object
+      },
+      computed: {
+         active(){
+            return this.$route.params.id === this.diary.id
+         },
+         suffix(){
+            return this.active? '_white': ''
+         }
       }
    }
 </script>
