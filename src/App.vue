791098e6bc9aaeb2390113db85089e34124d9ac2
邴新科 <kylebing@163.com>
2021-08-24 14:41:26 +0800

    自动切换路线

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 <script>
 export default {
    mounted() {
+       if(process.env.NODE_ENV !== 'development'){
+           if (innerWidth < 1024){
+               location = '/diary/'
+           }
+       }
       window.onresize = () => {
          this.$store.commit('setHeightPanel', window.innerHeight - 45)
          this.$store.commit('setHeightWindow', window.innerHeight)
