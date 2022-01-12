1808cbe74a634b5507eb003f5497255fc22c65dc
邴新科 <kylebing@163.com>
2022-01-12 15:28:49 +0800

    fix: pack config

--- vue.config.js
+++ vue.config.js
@@ -20,22 +20,23 @@ module.exports = {
   },
 
   configureWebpack: config => {
-    let packTimeString = new Moment().format('YYYYMMDDHHmmss') // 打包时间
-    let plugins = []
-    plugins.push(
-        new FileManagerPlugin({
-          events: {
-            onEnd: {
-              // mkdir: ['./archive'], // 新建 ./archive 目录
-              archive: [
-                // 打包 ./dist 到 ./archive/dist-datetime.zip 压缩文件中，压缩包中不带 dist 外壳
-                {source: '../diary/', destination: `../diary-${packTimeString}.zip`},
-              ]
+    if (process.env.NODE_ENV === 'production'){
+      let packTimeString = new Moment().format('YYYYMMDDHHmmss') // 打包时间
+      let plugins = []
+      plugins.push(
+          new FileManagerPlugin({
+            events: {
+              onEnd: {
+                // mkdir: ['./archive'], // 新建 ./archive 目录
+                archive: [
+                  // 打包 ./dist 到 ./archive/dist-datetime.zip 压缩文件中，压缩包中不带 dist 外壳
+                  {source: '../diary/', destination: `../diary-${packTimeString}.zip`},
+                ]
+              }
             }
-          }
-        })
-    )
-    config.plugins = config.plugins.concat(plugins) // 将新建的 plugin 添加到原 config 中的 plugin 中
+          })
+      )
+      config.plugins = config.plugins.concat(plugins) // 将新建的 plugin 添加到原 config 中的 plugin 中
+    }
   }
-
 }
