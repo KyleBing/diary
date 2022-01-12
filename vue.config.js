c0599af488d1a20b2d3ab0dc5f0ddf21f11dadf1
邴新科 <kylebing@163.com>
2022-01-12 11:03:26 +0800

    添加打包选项

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+const Moment = require('moment')
+
 module.exports = {
   publicPath: './',
   productionSourceMap: false, // 不产出 .map 文件
@@ -14,5 +17,25 @@ module.exports = {
         }
       },
     }
+  },
+
+  configureWebpack: config => {
+    let packTimeString = new Moment().format('YYYYMMDDHHmmss') // 打包时间
+    let plugins = []
+    plugins.push(
+        new FileManagerPlugin({
+          events: {
+            onEnd: {
+              // mkdir: ['./archive'], // 新建 ./archive 目录
+              archive: [
+                // 打包 ./dist 到 ./archive/dist-datetime.zip 压缩文件中，压缩包中不带 dist 外壳
+                {source: '../diary/', destination: `../diary-${packTimeString}.zip`},
+              ]
+            }
+          }
+        })
+    )
+    config.plugins = config.plugins.concat(plugins) // 将新建的 plugin 添加到原 config 中的 plugin 中
   }
+
 }
