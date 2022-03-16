8ec5cd818c3a4462e3d1692e490db302b1d8b494
邴新科 <kylebing@163.com>
2022-03-16 11:43:45 +0800

    ^

--- src/page/About.vue
+++ src/page/About.vue
@@ -5,7 +5,7 @@
         <div class="author">
             <a href="https://kylebing.cn" class="social-link">🌖 开发者主页</a>
             <a href="mailto:kylebing@163.com">kylebing@163.com</a>
-            <a href="https://github.com/KyleBing/diary">GitHub.com</a>
+            <a href="https://github.com/KyleBing/diary-vue">GitHub.com</a>
             <a>·</a>
             <a href="">v7.24 @ 2022-03-01</a>
         </div>
