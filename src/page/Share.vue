7ad8ea33124489a56591d205c496b278334d549d
邴新科 <kylebing@163.com>
2022-04-08 09:56:02 +0800

    美化分享中的返回链接

--- src/page/Share.vue
+++ src/page/Share.vue
@@ -58,7 +58,7 @@
             </div>
 
         </div>
-        <router-link class="back-link" to="/">《标题日记》</router-link>
+        <router-link class="back-link" to="/"><img src="../assets/img/logo.svg" alt="logo">标题日记</router-link>
 
     </div>
 </template>
