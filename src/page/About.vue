649edff6d59bccdacc7868a1e4bdd510b0221cd9
邴新科 <kylebing@163.com>
2022-05-24 14:43:02 +0800

    update

--- src/page/About.vue
+++ src/page/About.vue
@@ -7,7 +7,7 @@
             <a href="mailto:kylebing@163.com">kylebing@163.com</a>
             <a href="https://github.com/KyleBing/diary-vue">GitHub.com</a>
             <a>·</a>
-            <a href="">v7.30 @ 2022-05-18</a>
+            <a href="">v7.31 @ 2022-05-24</a>
         </div>
     </div>
 </template>
