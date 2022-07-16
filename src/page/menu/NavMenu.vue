1e166769af0e93b8ff79b78ec898877f1ecff7b2
邴新科 <kylebing@163.com>
2022-07-16 11:14:49 +0800

    添加菜单动画

--- src/page/menu/NavMenu.vue
+++ src/page/menu/NavMenu.vue
@@ -1,77 +1,90 @@
 <template>
     <!-- MENU -->
-    <div class="menu-panel" id="menu-panel" v-show="menuShowed" :style="'height:' + insets.heightPanel + 'px'">
-        <div class="menu-list" v-show="menuListShowed" :style="'min-height:' + insets.heightPanel + 'px'">
-            <div class="menu-list-group">
-                <!--1. 搜索-->
-                <div class="menu-list-group-item" v-if="isInMobileMode" @click="menuListClicked('search')">搜索</div>
+    <transition
+        enter-active-class="animated-fast slideInLeft"
+        leave-active-class="animated-fast slideOutLeft"
+    >
+        <div class="menu-panel" id="menu-panel" v-if="menuShowed" :style="'height:' + insets.heightPanel + 'px'">
+            <div class="menu-list" v-show="menuListShowed" :style="'min-height:' + insets.heightPanel + 'px'">
+                <div class="menu-list-group">
+                    <!--1. 搜索-->
+                    <div class="menu-list-group-item" v-if="isInMobileMode" @click="menuListClicked('search')">搜索</div>
 
-                <!--2. 类别筛选-->
-                <div class="menu-list-group-item" @click="menuListClicked('category')">
-                    <div>类别筛选</div>
-                    <div class="category-indicator">
-                        <div :class="['item', 'category-shared', 'mr-2', {active: isFilterShared}]"></div> <!-- 共享小图标标识 -->
-                        <div :class="['item' ,{active: filteredCategories.some(category => category === item.name_en)}]"
-                             :style="filteredCategories.indexOf(item.name_en) > -1? `background-color: ${item.color}`:`border-color: ${item.color}`"
-                             v-for="(item, index) in categoryAll"
-                             :title="item.name_en"
-                             :key="index"></div>
+                    <!--2. 类别筛选-->
+                    <div class="menu-list-group-item" @click="menuListClicked('category')">
+                        <div>类别筛选</div>
+                        <div class="category-indicator">
+                            <div :class="['item', 'category-shared', 'mr-2', {active: isFilterShared}]"></div> <!-- 共享小图标标识 -->
+                            <div :class="['item' ,{active: filteredCategories.some(category => category === item.name_en)}]"
+                                 :style="filteredCategories.indexOf(item.name_en) > -1? `background-color: ${item.color}`:`border-color: ${item.color}`"
+                                 v-for="(item, index) in categoryAll"
+                                 :title="item.name_en"
+                                 :key="index"></div>
+                        </div>
+                    </div>
+
+                    <!--3. 年份筛选-->
+                    <div class="menu-list-group-item" @click="menuListClicked('year')">
+                        <div>年份筛选</div>
+                        <div class="addon">{{ dateFilter }}</div>
+                    </div>
+
+                    <!--4. 统计数据-->
+                    <div class="menu-list-group-item" @click="goToStatisticPage">统计数据</div>
+
+                    <!--4. 账单-->
+                    <div class="menu-list-group-item" @click="goToBillPage">账单</div>
+
+                    <!--5. 银行卡-->
+                    <div class="menu-list-group-item" @click="menuListClicked('bankCard')">银行卡</div>
+
+                    <!--6. 修改密码-->
+                    <router-link class="menu-list-group-item" to="/change-password">修改密码</router-link>
+
+                    <!--7. 关于-->
+                    <div class="menu-list-group-item" @click="menuListClicked('about')">关于</div>
+
+                    <!--8. 退出-->
+                    <div class="menu-list-group-item" @click="logout">退出</div>
+
+                </div>
+
+                <div class="user-info-panel">
+                    <div class="user-info">
+                        <div class="user">
+                            <p class="username">{{ userInfo.nickname }}</p>
+                            <p class="email">{{ userInfo.email }}</p>
+                        </div>
+                        <div class="separator"></div>
+                        <div v-if="statisticsCategory.shared > 0" class="statistics">
+                            <p>共享 {{ statisticsCategory.shared }} 篇</p>
+                            <p>总计 {{ statisticsCategory.amount }} 篇</p>
+                        </div>
                     </div>
                 </div>
-
-                <!--3. 年份筛选-->
-                <div class="menu-list-group-item" @click="menuListClicked('year')">
-                    <div>年份筛选</div>
-                    <div class="addon">{{ dateFilter }}</div>
-                </div>
-
-                <!--4. 统计数据-->
-                <div class="menu-list-group-item" @click="goToStatisticPage">统计数据</div>
-
-                <!--4. 账单-->
-                <div class="menu-list-group-item" @click="goToBillPage">账单</div>
-
-                <!--5. 银行卡-->
-                <div class="menu-list-group-item" @click="menuListClicked('bankCard')">银行卡</div>
-
-                <!--6. 修改密码-->
-                <router-link class="menu-list-group-item" to="/change-password">修改密码</router-link>
-
-                <!--7. 关于-->
-                <div class="menu-list-group-item" @click="menuListClicked('about')">关于</div>
-
-                <!--8. 退出-->
-                <div class="menu-list-group-item" @click="logout">退出</div>
-
             </div>
 
-            <div class="user-info-panel">
-                <div class="user-info">
-                    <div class="user">
-                        <p class="username">{{ userInfo.nickname }}</p>
-                        <p class="email">{{ userInfo.email }}</p>
-                    </div>
-                    <div class="separator"></div>
-                    <div v-if="statisticsCategory.shared > 0" class="statistics">
-                        <p>共享 {{ statisticsCategory.shared }} 篇</p>
-                        <p>总计 {{ statisticsCategory.amount }} 篇</p>
-                    </div>
-                </div>
-            </div>
+            <!-- category-->
+            <transition enter-active-class="animated-fast fadeIn" leave-active-class="animated-fast fadeOut">
+                <menu-category-selector v-if="categoryShowed"/>
+            </transition>
+
+            <!-- year selector -->
+            <transition enter-active-class="animated-fast fadeIn" leave-active-class="animated-fast fadeOut">
+                <year-selector v-show="yearShowed"/>
+            </transition>
+
+            <!-- bank card -->
+            <transition enter-active-class="animated-fast-fast fadeIn" leave-active-class="animated-fast-fast fadeOut">
+                <bank-card v-if="bankCardShowed"/>
+            </transition>
+
+            <!--about-->
+            <transition enter-active-class="animated-fast fadeIn" leave-active-class="animated-fast fadeOut">
+                <about v-show="aboutShowed"/>
+            </transition>
         </div>
-
-        <!-- category-->
-        <menu-category-selector v-if="categoryShowed"/>
-
-        <!-- year selector -->
-        <year-selector v-show="yearShowed"/>
-
-        <!-- bank card -->
-        <bank-card v-if="bankCardShowed"/>
-
-        <!--about-->
-        <about v-show="aboutShowed"/>
-    </div>
+    </transition>
 
 </template>
 
@@ -246,6 +259,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
 
 </style>
