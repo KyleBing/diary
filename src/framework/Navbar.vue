cacca3c22e47128cefee32a9fd1e45dd0827ce0a
KyleBing <kylebing@163.com>
2022-08-17 21:28:19 +0800

    dark: border-color

--- src/framework/Navbar.vue
+++ src/framework/Navbar.vue
@@ -33,26 +33,16 @@
                 </div>
 
                 <div class="btn-text-group">
-                    <div class="btn-text ml-5" v-show="!isMenuShowed"  v-if="!isInMobileMode"
-                         @click="switchToCategory('bill')">
-                        <span>账单</span>
-                    </div>
-                    <div class="btn-text" v-show="!isMenuShowed"  v-if="!isInMobileMode"
-                         @click="switchToCategory('work')">
-                        <span>工作</span>
-                    </div>
-                    <div class="btn-text" v-show="!isMenuShowed"  v-if="!isInMobileMode"
-                         @click="switchToCategory('other')">
-                        <span>其它</span>
-                    </div>
-                    <div class="btn-text" v-show="!isMenuShowed"  v-if="!isInMobileMode"
-                         @click="switchToCategory('all')">
-                        <span>全部</span>
-                    </div>
+                    <div class="btn-text ml-5" v-show="!isMenuShowed" v-if="!isInMobileMode"
+                         @click="switchToCategory('bill')">账单</div>
+                    <div class="btn-text" v-show="!isMenuShowed" v-if="!isInMobileMode"
+                         @click="switchToCategory('work')">工作</div>
+                    <div class="btn-text" v-show="!isMenuShowed" v-if="!isInMobileMode"
+                         @click="switchToCategory('other')">其它</div>
+                    <div class="btn-text" v-show="!isMenuShowed" v-if="!isInMobileMode"
+                         @click="switchToCategory('all')">全部</div>
                     <div :class="['btn-text', {active: isFilterShared}]" v-show="!isMenuShowed"  v-if="!isInMobileMode"
-                         @click="switchToCategory('shared')">
-                        <span>共享</span>
-                    </div>
+                         @click="switchToCategory('shared')">共享</div>
                 </div>
             </div>
 
