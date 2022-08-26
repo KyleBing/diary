2d70d9a9f81fa268c8193526e5200674a6973744
KyleBing <kylebing@163.com>
2022-08-26 23:37:38 +0800

    天气选择器，调整图标大小

--- src/page/About.vue
+++ src/page/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about" :style="'height:' + insets.heightPanel + 'px'">
         <div class="logo">
-            <img src="../assets/img/logo.svg" alt="LOGO">
+            <img src="../assets/img/logo-login.svg" alt="LOGO">
         </div>
         <h3 class="title">{{nameZh}}</h3>
         <h4 class="subtitle">{{description}}</h4>
@@ -45,6 +45,7 @@ export default {
     color: $bg-light;
     padding-top: 60px;
     .logo{
+        margin-bottom: 10px;
         display: flex;
         justify-content: center;
         img{
