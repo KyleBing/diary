98f19d81a9d43ff278ce55ad3b7b7cf3a1982189
KyleBing <kylebing@163.com>
2022-08-26 23:12:53 +0800

    improve menu-list-item

--- src/page/menu/MenuListItem.vue
+++ src/page/menu/MenuListItem.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="menu-list-item">
-        <div class="menu-list-item-title">
-            <div class="icon">
-                <img src="../../assets/img/logo-login.svg" alt="">
-            </div>
-            <div class="menu-title">{{ menuName }}</div>
+        <div class="menu-list-item-icon" v-if="icon">
+            <img src="../../assets/img/logo-login.svg" alt="">
         </div>
-
-        <slot></slot>
-        <div class="addon" v-if="addOnText">{{ addOnText }}</div>
+        <div class="menu-list-item-content">
+            <div class="title">{{ menuName }}</div>
+            <slot></slot>
+            <div class="addon" v-if="addOnText">{{ addOnText }}</div>
+        </div>
     </div>
 </template>
 
@@ -16,11 +15,11 @@
 
 export default {
     name: "MenuListItem",
-    data() {
-        return {
-        }
-    },
     props: {
+        icon:{
+            type: String,
+            default: 'yes'
+        },
         menuName:{
             type: String,
             default: '菜单'
@@ -30,12 +29,6 @@ export default {
             default: ''
         },
     },
-    mounted() {
-    },
-
-    methods: {
-
-    }
 }
 </script>
 
@@ -44,58 +37,60 @@ export default {
 $height-menu-list: 50px;
 
 .menu-list-item{
-    display: flex;
-    justify-content: space-between;
     align-items: center;
-    cursor: pointer;
-    position: relative;
-    font-size: $fz-menu;
-    height: $height-menu-list;
-    line-height: $height-menu-list;
-    padding: 0 15px;
-    color: white;
-    @extend .unselectable;
-    @include border-radius($radius-pc);
-    overflow: hidden;
-    &:after{
-        width: 100%;
-        bottom: 0;
-        left: 0;
-        content: '';
-        display: block;
-        background-color: $color-border-menu;
-        height: 1px;
-        position: absolute;
-        transform-origin: 0 100%;
-        transform: scaleY(0.5);
-    }
+    display: flex;
+    justify-content: flex-start;
+
     &:active{
         background-color: lighten($bg-menu,10%);
     }
-    .menu-list-item-title{
+
+    .menu-list-item-icon{
+        margin-right: 10px;
+        width: 30px;
+        height: 30px;
+        img{
+            display: block;
+            width: 100%;
+        }
+    }
+    .menu-list-item-content{
+        width: 100%;
         display: flex;
-        justify-content: flex-start;
+        justify-content: space-between;
         align-items: center;
-        .icon{
-            width: 30px;
-            height: 30px;
-            img{
-                display: block;
-                width: 100%;
-            }
+        cursor: pointer;
+        position: relative;
+        font-size: $fz-menu;
+        height: $height-menu-list;
+        line-height: $height-menu-list;
+        padding: 0 15px;
+        color: white;
+        @extend .unselectable;
+        @include border-radius($radius-pc);
+        overflow: hidden;
+        .addon{
+            margin-top: 2px; // 抵消靠上的显示错觉
+            font-size: $fz-list-content;
+            line-height: 1;
+            color: $text-menu-second;
         }
-        .menu-title{
-            margin-left: 10px;
 
+        &:after{
+            width: 100%;
+            bottom: 0;
+            left: 0;
+            content: '';
+            display: block;
+            background-color: $color-border-menu;
+            height: 1px;
+            position: absolute;
+            transform-origin: 0 100%;
+            transform: scaleY(0.5);
         }
     }
 
-    .addon{
-        margin-top: 2px; // 抵消靠上的显示错觉
-        font-size: $fz-list-content;
-        line-height: 1;
-        color: $text-menu-second;
-    }
+
 }
 
 </style>
