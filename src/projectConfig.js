e4a642fdf31af21d3c1cc1fdd579479c280bf03f
邴新科 <kylebing@163.com>
2023-01-04 14:28:20 +0800

    添加和风天气 key

--- src/projectConfig.js
+++ src/projectConfig.js
@@ -1,4 +1,10 @@
 export default {
-    QiniuImgBaseURL: 'http://rnov15v13.hb-bkt.clouddn.com/', // 最后面带 `/`
+    // 七牛云
+    // 地址： https://portal.qiniu.com/kodo/overview
+    QiniuImgBaseURL: 'http://rnov15v13.hb-bkt.clouddn.com/', // 空间域名，最后面带 `/`
     QiniuBucketName: 'diary-container', // 七牛云对象存储空间的名称
+
+    // 和风天气开发 key
+    // 地址：https://console.qweather.com/
+    HefengWeatherKey: '和风天气的开发key',
 }
