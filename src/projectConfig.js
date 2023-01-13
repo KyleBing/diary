e88156cb761926f10fab261dd240ae3fb24a507e
KyleBing(MBP14) <kylebing@163.com>
2023-01-13 23:08:57 +0800

    update image container url

--- src/projectConfig.js
+++ src/projectConfig.js
@@ -1,7 +1,7 @@
 export default {
     // 七牛云
     // 地址： https://portal.qiniu.com/kodo/overview
-    QiniuImgBaseURL: 'http://rnov15v13.hb-bkt.clouddn.com/', // 空间域名，最后面带 `/`
+    QiniuImgBaseURL: 'http://diary-container.kylebing.cn/', // 空间域名，最后面带 `/`
     QiniuBucketName: 'diary-container', // 七牛云对象存储空间的名称
 
     // 和风天气开发 key
