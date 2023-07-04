fabff8781f2cb6b99c02bb19d45bfac670e1dc2a
邴新科-工作 <kylebing@163.com>
2023-07-04 09:22:14 +0800

    fix 没有头像时显示问题

--- src/page/menu/UserProfile.vue
+++ src/page/menu/UserProfile.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="user-profile">
         <div class="avatar">
-            <img :src="userInfo.avatar + '-' + QiniuStyleSuffix || icons.logoIcon.login" alt="Avatar">
+            <img v-if="userInfo.avatar" :src="userInfo.avatar + '-' + QiniuStyleSuffix || icons.logoIcon.login" alt="Avatar">
+            <img v-else src="../../assets/img/logo/logo_avatar.svg" alt="Avatar">
         </div>
         <div class="user-info mt-1 mb-4">
             <div class="user">
