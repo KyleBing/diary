b32c754f8abc87864f20288f8232b6f1c2fa8262
KyleBing <kylebing@163.com>
2023-11-10 09:19:08 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cd /usr/share/nginx/html &&
-rm -Rf diary/* &&
-mv diary-* diary &&
-cd diary &&
-unzip diary-* &&
-rm -f diary-*
-echo 'Diary deploy finished.'
+if compgen -G "./diary*.zip" > /dev/null; then
+    rm -Rf diary/* &&
+    mv diary-* diary &&
+    cd diary &&
+    unzip diary-* &&
+    rm -f diary-*
+    echo 'Diary deploy finished.'
+else
+    echo "diary-*.zip 不存在"
+fi
+
+
