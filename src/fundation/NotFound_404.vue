9bba0c8d2a951ab81d3aa09af32b43c67c1849f1
KyleBing <kylebing@163.com>
2023-11-11 10:47:46 +0800

    404 logo

--- src/fundation/NotFound_404.vue
+++ src/fundation/NotFound_404.vue
@@ -2,10 +2,10 @@
     <div class="bg" :style="`height: ${insets.windowsHeight}px`">
         <div class="not-found">
             <div class="logo">
-                <img :src="icons.logo" alt="LOGO">
+                <img src="../assets/img/logo/logo_server_error.svg" alt="logo error">
             </div>
             <h1>访问的页面不存在</h1>
-            <p><router-link to="/">返回主页</router-link></p>
+            <h1><router-link to="/">返回主页</router-link></h1>
         </div>
     </div>
 </template>
@@ -29,7 +29,9 @@ export default {
 
 <style scoped lang="scss">
 @import "../scss/plugin";
+
 .bg{
+    letter-spacing: 1px;
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
@@ -53,14 +55,14 @@ a{
     justify-content: center;
     flex-flow: column nowrap;
     h1{
-        font-size: $fz-title;
+        font-size: $fz-main;
         font-weight: normal;
         margin-bottom: 10px;
     }
     .logo{
         img{
             display: block;
-            width: 100px;
+            width: 80px;
         }
     }
 }
