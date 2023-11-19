b468e106797ffc9f7ed7a4f617ea6e9d85939692
KyleBing(MBP14) <kylebing@163.com>
2023-11-19 20:03:46 +0800

    修改 avatar 上传后台路径

--- src/api/fileApi.js
+++ src/api/fileApi.js
@@ -1,5 +1,5 @@
 import request from "../request";
 
 export default {
-    getUploadToken(params) {return request('get', params, null,  '/file/')},
+    getUploadToken(params) {return request('get', params, null,  '/image-qiniu/')},
 }
