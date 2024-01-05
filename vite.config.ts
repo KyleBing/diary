8aa27295c858bf6fc9f4eb4b7414c9a5cd2ed3b2
KyleBing <kylebing@163.com>
2024-01-05 14:30:56 +0800

    vite pwa 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from "vite-svg-loader"
+import { VitePWA } from 'vite-plugin-pwa'
+
 import { resolve } from 'path'
 
 
@@ -8,7 +10,39 @@ import { resolve } from 'path'
 export default defineConfig({
     plugins: [
         vue(),
-        svgLoader()
+        svgLoader(),
+        VitePWA({
+            injectRegister: 'auto',
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true // 是否在开发模式下也启用 pwa 配置
+            },
+
+            // MANIFEST PWA https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements.html
+            includeAssets: ['logo.svg', 'apple-touch-icon.png', 'mask-icon.svg', 'favicon.png'],
+            manifest: {
+                name: '标题日记',
+                short_name: "日记",
+                theme_color: "#373737",
+                start_url: "./",
+                display: "standalone",
+                background_color: "#373737",
+                icons: [
+                    {
+                        src: "logo.svg",
+                        sizes: "512x512",
+                        type: "image/svg+xml",
+                        purpose: "any",
+                    },
+                    {
+                        src: "appicon-apple.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                ],
+            },
+        })
     ],
     resolve: {
         alias: {
