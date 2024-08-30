85b7f47794fb44cbc341f4e0dd8b3b49f690c22a
KyleBing <kylebing@163.com>
2024-08-30 15:55:44 +0800

    fix 移动端上面的 Logo 类型

--- src/framework/navbar/Navbar.vue
+++ src/framework/navbar/Navbar.vue
@@ -121,10 +121,10 @@
                      v-if="route.name === 'Edit' || route.name === 'EditNew'"
                      alt="LOGO">
                 <a v-else-if="storeProject.listStyle === EnumListStyle.list">
-                    <img :src="SVG_ICONS.logo_icons.logo_content" alt="日记">
+                    <img :src="SVG_ICONS.logo_icons.logo" alt="日记">
                 </a>
                 <a v-else>
-                    <img :src="SVG_ICONS.logo_icons.logo" alt="日记">
+                    <img :src="SVG_ICONS.logo_icons.logo_content" alt="日记">
                 </a>
             </div>
 
